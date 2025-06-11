8c67f2af6c78a95d2335771f41c45d591c9efafd
iankaz <iankazembe41@gmail.com>
2025-06-11 02:44:24 +0200

    ...

--- validation/journalValidation.js
+++ validation/journalValidation.js
@@ -16,11 +16,20 @@ const journalSchema = Joi.object({
     .messages({
       "any.only": "Mood must be one of: happy, sad, angry, neutral, excited, anxious",
     }),
+  weather: Joi.string()
+    .valid("sunny", "cloudy", "rainy", "snowy", "stormy", "foggy", "windy")
+    .required()
+    .messages({
+      "any.only": "Weather must be one of: sunny, cloudy, rainy, snowy, stormy, foggy, windy",
+    }),
+  tags: Joi.array().items(Joi.string().max(50)).default([]),
+  isPrivate: Joi.boolean().default(false),
+  location: Joi.string().max(100).allow(""),
   date: Joi.date().default(() => new Date()),
 })
 
 const journalUpdateSchema = journalSchema.fork(
-  ["title", "content", "mood", "date"],
+  ["title", "content", "mood", "weather", "tags", "isPrivate", "location", "date"],
   (schema) => schema.optional()
 )
 
